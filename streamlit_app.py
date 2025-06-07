27f3e3d13401a5ffd1836369165cee0fb4ca2668
man2720 <aymanzabero@gmail.com>
2025-06-07 12:54:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import streamlit as st
 st.title("--Halo..--")
 st.write("Tugas Informatika Vinza .")
 st.image("IMG-20250604-WA0052.jpg",width=500)
-
 st.title("Aplikasi Sederhana")
 st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
 angka = st.number_input("Tulis Sebuah Angka:", value=0, step=1)
